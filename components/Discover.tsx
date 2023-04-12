d2157d781903d4f67b44e5ce57f22fcad4bf1c60
Addie <addie@aiii.ai>
2023-04-13 00:44:35 +0800

    discover and footer

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { topics } from '@/utils/constants'
 
 const Discover = () => {
+
+  // get data through url
+  const router = useRouter()
+  console.log(router)
+  const { topic } = router.query
+
+  const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex item-center gap-2 justify-center cursor-pointer text-black"
+
+  const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex item-center gap-2 justify-center cursor-pointer text-[#F51997]"
+
   return (
-    <div>Discover</div>
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="text-gray-500 font-semivold m-3 mt-4 hidden xl:block">
+        Popular Topics
+      </p>
+      <div className="flex gap-3 flex-wrap">
+        {topics.map((item) => (
+          <Link href={`/?topic=${item.name}`} key={item.name}>
+            <div className={topic === item.name ? activeTopicStyle : topicStyle}>
+              <span className="font-bold text-2xl xl:text-md">{item.icon}</span>
+              <span className="font-medium text-md hidden xl:block capitalize">
+                {item.name}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { footerList1, footerList2, footerList3 } from '@/utils/constants'
 
 const Footer = () => {
+
+  const FooterList = ({items, mt}: {items:string[], mt:boolean}) => {
+    return (
+      <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+        {items.map((item) => (
+          <p
+            key={item}
+            className="text-gray-400 text-sm hover:underline cursor-pointer"
+          >
+            {item}
+          </p>
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <div>Footer</div>
+    <div className="mt-6 hidden xl:block">
+      <div className="flex flex-wrap gap-2 mt-5">
+        <FooterList items={footerList1} mt={false}/>
+        <FooterList items={footerList2} mt/>
+        <FooterList items={footerList3} mt/>
+        <p className='text-gray-400 text-sm mt-5'>2023 TokTok</p>
+      </div>
+    </div>
   )
 }
 
