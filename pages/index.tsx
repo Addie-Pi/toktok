a1ad2fb8e49c7aa11b6193aab56ff4fb5788ec51
Addie <addie@aiii.ai>
2023-04-14 01:03:57 +0800

    connect to server and get first data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import axios from 'axios';
+import { Video } from '../types'
 
-const inter = Inter({ subsets: ['latin'] })
+interface IProps{
+  videos: Video[]
+}
 
-export default function Home() {
+const Home = ({ videos }: IProps) => {
+  console.log("videos ", videos)
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-  )
+   )
 }
+
+// Next.js will pre-render this page on each request using the data returned by getServerSideProps
+export const getServerSideProps = async() => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
+  
+  return {
+    props:{
+      videos: data
+    }
+  }
+}
+
+export default Home;
