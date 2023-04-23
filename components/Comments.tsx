b1cbcf8c1c2a825a82f5e4b34dc5fa05a7a27eea
Addie <addie@aiii.ai>
2023-04-24 00:18:09 +0800

    post comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -6,6 +6,7 @@ import { GoVerified } from 'react-icons/go'
 import userAuthStore from '@/store/auth.store'
 import NoResult from './NoResult'
 import comment from '@/sanity-backend/schemas/comment'
+import { IUser } from '@/types'
 
 interface IProps {
   isPostingComment: Boolean
@@ -29,13 +30,50 @@ interface IComment{
 
 const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
  
-  const userProfile = userAuthStore()
+  const {userProfile, allUsers} = userAuthStore()
  
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
         {comments?.length ? (
-          <div>videos</div>
+          comments.map((item, index) => (
+            <>
+              {allUsers.map(
+                (user: IUser) =>
+                  user._id === item.postedBy._ref && (
+                    <div className="p-2 items-center" key={index}>
+                      <Link href={`/profile/${user._id}`}>
+                        <div className="flex items-start gap-3">
+                          <div className="w-8 h-8">
+                            <Image
+                              src={user.image}
+                              width={34}
+                              height={34}
+                              className="rounded-full"
+                              alt="user profile"
+                            />
+                          </div>
+                          <div className="hidden xl:block">
+                            <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                              {user.userName.replaceAll(' ', '')}
+                              <GoVerified className="text-blue-400" />
+                            </p>
+                            <p className="capitalize text-gray-400 text-xs">
+                              {user.userName}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
+                      
+                      {/* display the comment */}
+                      <div>
+                        <p>{item.comment}</p>
+                      </div>
+                    </div>
+                  )
+              )}
+            </>
+          ))
         ) : (
           <NoResult text="No comments yet!" />
         )}
--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {use, useEffect} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { GoVerified } from 'react-icons/go'
@@ -16,29 +16,39 @@ const SuggestedAccounts = () => {
 
 
   return (
-    <div className='xl:border-b-2 border-gray-200 pb-4'>
-      <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>Suggested Accounts</p>
+    <div className="xl:border-b-2 border-gray-200 pb-4">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
+        Suggested Accounts
+      </p>
       <div>
-        {
-          allUsers.slice(0, 6).map((user: IUser)=>(
-            <Link href={`/profile/${user._id}`} key={user._id}>
-              <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded'>
-                <div className='w-8 h-8'>
-                  <Image 
-                    src={user.image}
-                    width={34}
-                    height={34}
-                    className='rounded-full'
-                    alt='user profile'
-                  />
-                </div>
-                <div className='hidden xl:block'>
-                  <p></p>
-                </div>
+        {allUsers.slice(0, 6).map((user: IUser) => (
+          <Link
+            
+            href={`/profile/${user._id}`}
+            key={user._id}
+          >
+            <div className="flex items-center gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
+              <div className="w-8 h-8">
+                <Image
+                  src={user.image}
+                  width={34}
+                  height={34}
+                  className="rounded-full"
+                  alt="user profile"
+                />
               </div>
-            </Link>
-          ))
-        }
+              <div className="hidden xl:block">
+                <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                  {user.userName.replaceAll(' ', '')}
+                  <GoVerified className="text-blue-400" />
+                </p>
+                <p className='capitalize text-gray-400 text-xs'>
+                  {user.userName}
+                </p>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
